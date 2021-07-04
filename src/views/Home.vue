71af4e0a9c80b1bd599edabf3ec1c68d1bd10d4d
MardonioMelo <mardonio.quimico@gmail.com>
2021-07-03 22:51:10 -0300

    Foram criados os componentes Banner e Form

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,21 @@
 <template>
+  <BannerItem />
   <div class="main-container">
-   <h1>Home</h1>
+    <h1>Monte seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import BannerItem from "../components/BannerItem.vue";
+import BurgerForm from "../components/BurgerForm.vue";
+
 export default {
-  name: "Home"
-}
+  name: "Home",
+  components: {
+    BannerItem,
+    BurgerForm,
+  },
+};
 </script>
+
